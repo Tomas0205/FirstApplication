393b374b90fc80568841ed14ac9bdcfb57a63a2b
Anderson Araujo <adearaujor@hotmail.com>
2020-10-15 16:03:44 +0100

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    /*
+     Google->
+    Que son Objetos, Metodos, Variables, Condiciones, eventos?
+    Softwarepattern
+     */
+    //Esto es un comentario
+    //Esto es una clase
+    class Program
+    {
+        //Esto es un metodo
+        static void Main(string[] args)
+        {
+            //Test
+            Console.WriteLine("Hola Mundo");
+            Console.ReadLine();
+        }
+    }
+}
